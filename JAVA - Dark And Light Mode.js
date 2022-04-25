95c9b7558dec4bbe7fbed463c9e13166bd0fd928
WiredrawnBoar <99906744+WiredrawnBoar@users.noreply.github.com>
2022-04-25 12:06:55 +0100

    Add files via upload

---
+++ JAVA - Dark And Light Mode.js
@@ -0,0 +1,9 @@
+function _dark(){
+document.body.style.background="black";
+document.body.style.color="grey";
+}
+
+function _light(){
+    document.body.style.background="white";
+    document.body.style.color="black"; 
+}
